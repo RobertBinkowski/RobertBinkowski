2c338a7b8bf4de3e61931abb07b1e0fe9727b847
Robert Binkowski <56690174+RobertBinkowski@users.noreply.github.com>
2022-12-02 18:52:55 +0000

    small changes

--- src/views/ExperienceSection.vue
+++ src/views/ExperienceSection.vue
@@ -1,44 +1,102 @@
 <template>
-    <section id="experienceSection">
-        <h2>Some Work I Have Done</h2>
-        <div class="experience">
-            <a href="https://sykownochata.pl/" target="_blank">
-                <div>
-                    <p>
-                        <img src="../assets/logo/experience/logo_2.png" alt="SykownoChataLogo" class="exp-logo"><br>
-                        <strong>Business Website</strong><br>
-                        <em>Sykowno Chata - Poland</em><br>
-                        Summer 2019 
-                    </p>
-                    <br>
-                    <p>
-                        <em>Responsive</em>, <em>Mobile First</em> Website designed to help advertise customers apartments.
-                    </p>
-                    <div class="grid">
-                        <em class="fa-brands fa-html5"></em>
-                        <em class="fa-brands fa-css3-alt"></em>
-                        <em class="fa-brands fa-php"></em>
-                        <em class="fa-brands fa-js"></em>
-                    </div>
-                </div>
-                <div>
-                    <img src="../assets/logo/experience/splashScreen.png" alt="Website Screenshot" class="exp-screen">
-                </div>
-            </a>
+  <section id="experienceSection">
+    <h2>Work</h2>
+    <div class="experience" v-for="job in jobs" :key="job">
+      <a :href="job.link" target="_blank">
+        <div>
+          <p>
+            <img
+              :src="job.logo"
+              v-show="job.logo"
+              :alt="job.name + ' Icon'"
+              class="exp-logo"
+            /><br />
+            <strong>{{ job.role }}</strong
+            ><br />
+            <em>{{ job.name }} - {{ job.location }}</em
+            ><br />
+            {{ job.period }}
+          </p>
+          <br />
+          <p>
+            {{ job.details }}
+          </p>
+          <div class="grid">
+            <em v-for="icon in icons" :key="icon" :class="icon.icon"></em>
+          </div>
         </div>
-    </section>
+        <div>
+          <img
+            v-show="job.img"
+            :src="job.img"
+            :alt="job.name + ' Screenshot'"
+            class="exp-screen"
+          />
+        </div>
+      </a>
+    </div>
+  </section>
 </template>
 
 <style lang="scss">
-@import "../assets/scss/variables";
-#experienceSection{
-    a{
-        .exp-logo{
-            height: 5em;
-        }
+  @import "../assets/scss/variables";
+
+  #experienceSection {
+    a {
+      .exp-logo {
+        height: 3em;
+      }
     }
-    .fa-brands{
-        font-size: 2em;
+
+    .fa-brands {
+      font-size: 2em;
     }
-}
+  }
 </style>
+
+<script>
+  export default {
+    data() {
+      return {
+        jobs: [
+          {
+            enabled: true,
+            name: "ID-Pal",
+            role: "Junior PHP Developer",
+            link: "https://www.id-pal.com/",
+            location: "Ireland",
+            logo: "",
+            period: "November 2022 - Current",
+            details:
+              "Responsive, Mobile First Website designed to help advertise customers apartments.",
+            img: "",
+            icons: [
+              { name: "HTML", icon: "fa-brands fa-html5" },
+              { name: "SCSS", icon: "fa-brands fa-css3-alt" },
+              { name: "PHP", icon: "fa-brands fa-php" },
+              { name: "JavaScript", icon: "fa-brands fa-js" },
+            ],
+          },
+          {
+            enabled: true,
+            name: "Sykowno Chata",
+            role: "Full Stack Developer",
+            link: "https://sykownochata.pl/",
+            location: "Poland",
+            logo: "../assets/logo/experience/logo_2.png",
+            period: "June 2022 - October 2022",
+            details:
+              "Responsive, Mobile First Website designed to help advertise customers apartments.",
+            img: "../assets/logo/experience/splashScreen.png",
+            icons: [
+              { name: "HTML", icon: "fa-brands fa-html5" },
+              { name: "SCSS", icon: "fa-brands fa-css3-alt" },
+              { name: "PHP", icon: "fa-brands fa-php" },
+              { name: "JavaScript", icon: "fa-brands fa-js" },
+            ],
+          },
+        ],
+      };
+    },
+  };
+</script>
