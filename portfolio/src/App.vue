95dd5d27e8e5738d27ebd23b73197fe582be05c8
Robert Binkowski <56690174+RobertBinkowski@users.noreply.github.com>
2023-05-22 11:37:57 +0100

    footer added

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -1,79 +1,53 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import FooterBar from './components/FooterBar.vue'
 </script>
 
 <template>
-  <header>
+  <header id="nav">
     <img alt="Logo" class="logo" src="@/assets/logo.png" />
+
+    <!-- Routes -->
     <nav>
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/about">About</RouterLink>
     </nav>
+    <!-- End Routes -->
+    <!-- Other Links -->
+    <div class="links">
+      <a href="">LinkedIn</a>
+      <a href="">LinkedIn</a>
+      <a href="">LinkedIn</a>
+    </div>
+    <!-- End -->
   </header>
-
   <RouterView />
+  <FooterBar />
 </template>
 
 <style scoped>
 header {
   line-height: 1.5;
-  max-height: 100vh;
+  width: 100vw;
+  padding: 1em;
+  display: block;
+  max-width: 1000px;
+  background-color: black;
 }
 .logo {
   display: block;
   height: 2em;
-  /* margin: 0 auto 2rem; */
 }
-
-nav {
+.route-active {
+  color: red;
+}
+#nav .links {
+  padding-top: 1em;
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
 }
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+#nav .links a {
+  padding: 1em;
 }
 </style>
+
+<script lang="ts"></script>
---
+++ portfolio/src/components/FooterBar.vue
@@ -0,0 +1,71 @@
+<template>
+  <footer>
+    <!-- <logoSVG /> -->
+    <br />
+    <ul id="contact">
+      <a
+        v-for="contact in contacts"
+        :key="contact"
+        :href="contact.link"
+        target="_self"
+        v-show="contact.enabled"
+      >
+        <li>
+          <p :class="contact.icon"></p>
+        </li>
+      </a>
+    </ul>
+    <br />
+    <p>
+      This website was designed and built by <br />
+      <strong>Robert Binkowski</strong>
+    </p>
+  </footer>
+</template>
+
+<!-- <style lang="scss">
+@import '../assets/scss/variables';
+
+footer {
+  padding-top: 4em;
+  padding-bottom: 3em;
+  min-height: 10vh;
+  li {
+    font-size: 1.3em;
+  }
+  #contact {
+    p {
+      font-size: 1.2em;
+    }
+
+    &::after {
+      content: '';
+      background-color: $acc-2;
+      height: 100px;
+      display: block;
+      width: 1px;
+      margin: 0px auto -15px auto;
+    }
+  }
+  li:hover {
+    margin-top: -0.2em;
+    margin-bottom: 0.2em;
+  }
+}
+</style> -->
+
+<script>
+// import LogoSVG from "./SVGs/LogoSVG.vue";
+
+export default {
+  name: 'app',
+  components: {
+    // LogoSVG,
+  },
+  props: {
+    contacts: {
+      required: true
+    }
+  }
+}
+</script>
