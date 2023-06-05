166ac41ace4d16e875ab6a57c167bf272b208a8c
Robert Binkowski <56690174+RobertBinkowski@users.noreply.github.com>
2023-06-05 22:48:59 +0100

    finished timeline object

--- portfolio/src/components/Timeline/TimelineObject.vue
+++ portfolio/src/components/Timeline/TimelineObject.vue
@@ -1,12 +1,8 @@
 <template>
   <div class="timelineObject">
     <div class="left">
-      <div class="timelineLogo">
-        <img :src="article.logo" :alt="article.name" />
-      </div>
-      <div class="timelineDate">
-        <p>{{ article.date_started }} - {{ article.date_ended }}</p>
-      </div>
+      <img class="timelineLogo" :src="article.logo" :alt="article.name" />
+      <p class="timelineDate">{{ article.date_started }} - {{ article.date_ended }}</p>
     </div>
     <div class="right">
       <div class="timelineDetails">
@@ -21,15 +17,10 @@
 
 <script>
 export default {
-  data() {
-    return {
-      article: {
-        logo: '../../../public/logo.png',
-        title: 'My first article',
-        content: 'This is my first article',
-        date_started: '2019-01',
-        date_ended: '2019-01'
-      }
+  props: {
+    article: {
+      type: Object,
+      required: true
     }
   }
 }
@@ -40,31 +31,26 @@ export default {
 
 .timelineObject {
   display: flex;
+  justify-content: space-around;
   width: 100%;
+
   .left {
     display: flex;
-    text-align: left;
+    align-items: center;
+    position: relative;
+    left: 2em;
     .timelineLogo {
       overflow: hidden;
-      margin: 1em;
-      img {
-        filter: drop-shadow(0px 0px 5px $acc-1);
-        width: 100%;
-        height: 3em;
-        object-fit: cover;
-      }
+      filter: drop-shadow(0px 0px 5px $acc-1);
+      height: 3em;
+      object-fit: cover;
     }
     .timelineDate {
-      display: flex;
-      align-items: center;
-      p {
-        font-size: 0.9em;
-        color: $acc-2-d;
-      }
+      font-size: 0.9em;
+      color: $acc-2-d;
     }
   }
   .timelineDetails {
-    display: flex;
     text-align: center;
     min-width: 20em;
     background-color: $acc-1-dd;
--- portfolio/src/views/TimelinePage.vue
+++ portfolio/src/views/TimelinePage.vue
@@ -2,17 +2,41 @@
   <main>
     <article>
       <!-- <h1>Timeline Page</h1> -->
-      <TimelineObject />
+      <TimelineObject v-for="article in articles" :key="article" :article="article" />
     </article>
   </main>
 </template>
 
 <script lang="ts">
-import TimelineObject from '../components/Timeline/TimelineSuspense.vue'
+import TimelineObject from '../components/Timeline/TimelineObject.vue'
 
 export default {
   components: {
     TimelineObject
+  },
+  data() {
+    return {
+      articles: [
+        {
+          logo: '../../../public/logo.png',
+          title: 'My first article',
+          content:
+            'This is my first article, lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, eget aliq',
+          date_started: '2019-01',
+          date_ended: '2019-01'
+        },
+        {
+          logo: '../../../public/logo.png',
+          title: 'My first article',
+          content:
+            'This is my first article, lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, eget aliq',
+          date_started: '2019-01',
+          date_ended: '2019-01'
+        }
+      ]
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped></style>
