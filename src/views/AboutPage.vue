847f0c8a922d1cab7cc9ac032de890d4597f2d48
Robert Binkowski <56690174+RobertBinkowski@users.noreply.github.com>
2023-06-18 00:13:38 +0100

    image component initial work

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -1,9 +1,26 @@
 <template>
   <main>
     <h1>About Page</h1>
+    <imageComponent
+      image="https://bulma.io/images/placeholders/1280x960.png"
+      alt="Placeholder
+    image"
+      caption="Placeholder image"
+    ></imageComponent>
   </main>
 </template>
 
-<script lang="ts"></script>
+<script lang="ts">
+import imageComponent from '@/components/components/imageComponent.vue'
 
-<style lang="scss" scoped></style>
+export default {
+  name: 'AboutPage',
+  components: {
+    imageComponent
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/variables';
+</style>
