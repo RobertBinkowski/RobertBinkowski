3d5b62f3eefc7bfea427ac085a02beb6fb5ff5d7
Robert Binkowski <56690174+RobertBinkowski@users.noreply.github.com>
2023-06-18 19:08:56 +0100

    removed unused code

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -1,23 +1,13 @@
 <template>
   <main>
     <h1>About Page</h1>
-    <imageComponent
-      image="https://bulma.io/images/placeholders/1280x960.png"
-      alt="Placeholder
-    image"
-      caption="Placeholder image"
-    ></imageComponent>
   </main>
 </template>
 
 <script lang="ts">
-import imageComponent from '@/components/components/imageComponent.vue'
-
 export default {
   name: 'AboutPage',
-  components: {
-    imageComponent
-  }
+  components: {}
 }
 </script>
 
