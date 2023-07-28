fbc067854715fc58f99884931485eab646eff4f9
Robert Binkowski <56690174+RobertBinkowski@users.noreply.github.com>
2023-07-28 18:19:59 +0100

    projects section finished

--- src/components/HomePage/ProjectsSection.vue
+++ src/components/HomePage/ProjectsSection.vue
@@ -1,87 +1,25 @@
 <template>
   <section id="projectsSection">
-    <h2>Some Projects I Have Done</h2>
-    <div class="experience" v-for="project in projects" :key="project.id" v-show="project.enabled">
-      <a :href="project.link" target="_blank">
-        <div>
-          <p class="fa-solid fa-folder-open left"></p>
-          <div class="right">
-            <p class="fa-brands fa-github"></p>
-            <p class="fa-solid fa-arrow-up-right-from-square"></p>
-          </div>
-          <h3>{{ project.name }}</h3>
-          <p>
-            {{ project.value }} -
-            <!-- <small
-              ><em>{{ project.date }}</em></small
-            > -->
-          </p>
-          <div class="grid technology">
-            <em
-              v-for="tech in project.technologies"
-              :key="tech.id"
-              :title="tech.name"
-              :class="tech.icon"
-            ></em>
-          </div>
-        </div>
-      </a>
-    </div>
+    <h2>Projects</h2>
+    <projectCardComponent v-for="project in projects" :key="project" :project="project" v-show="project.enabled">
+    </projectCardComponent>
   </section>
 </template>
 
 <style lang="scss">
 @import '../../assets/scss/variables';
 @import '../../assets/scss/mixins';
-#projectsSection {
-  .experience {
-    div {
-      position: relative;
-      .left {
-        position: absolute;
-        top: 0em;
-        left: 1em;
-        font-size: 2em;
-      }
-      .right {
-        position: absolute;
-        top: -1em;
-        right: 1em;
-        font-size: 1.2em;
-      }
-      .technology {
-        font-size: 2em;
-      }
-    }
-  }
-  .experience {
-    padding: 1em;
-    transition: $tr-s;
-    div {
-      padding: 1em;
-    }
-    &:hover {
-      @include gradient();
-      .exp-screen {
-        filter: grayscale(0);
-        border-color: $acc-2;
-        border-radius: 0.1em;
-      }
-    }
-    .exp-screen {
-      width: calc(14rem + 15vw);
-      border-radius: 1em;
-      border: 1px solid #ffffff00;
-      filter: grayscale(100%);
-      transition: $tr-s;
-    }
-  }
-}
 </style>
 
-<script>
+<script lang="js">
+
+import projectCardComponent from '../components/projectCardComponent.vue'
+
 export default {
   name: 'ProjectsSection',
+  components: {
+    projectCardComponent
+  },
   props: {
     projects: {
       type: Array,
---
+++ src/components/components/projectCardComponent.vue
@@ -0,0 +1,94 @@
+<template>
+  <a class="card-content" :href="project.id ? 'project/' + project.id : ''">
+    <div class="icons">
+      <p class="fa-solid fa-folder-open left"></p>
+      <a :href="project.link" class="right" target="_blank">
+        <p class="fa-brands fa-github"></p>
+      </a>
+    </div>
+    <div class="mainContent">
+      <h3>{{ project.name }}</h3>
+      <p>
+        {{ project.value }}
+      </p>
+      <small><em v-show="false">{{ project.date }}</em></small>
+    </div>
+    <div class="technologies">
+      <tagComponent v-for="tag in project.technologies" :key="tag" :tag="tag"></tagComponent>
+    </div>
+  </a>
+</template>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/_variables.scss';
+@import '../../assets/scss/mixins';
+
+.card-content {
+  widows: 100%;
+  padding: 1em 0 1em 0;
+
+  .icons {
+    position: relative;
+
+    .left {
+      position: absolute;
+      top: -1em;
+      left: 0em;
+      font-size: 1.5em;
+    }
+
+    .right {
+      position: absolute;
+      top: -1em;
+      right: 1em;
+      font-size: 1.5em;
+    }
+  }
+
+  &:hover {
+    @include gradient();
+  }
+
+  a {
+    text-decoration: none;
+
+    &:hover {
+      color: $acc-2;
+    }
+  }
+
+  h3 {
+    margin: 0.5em 0;
+  }
+
+  p {
+    // color: white;
+    margin: 0.5em 0;
+  }
+
+  .technologies {
+    padding-top: 2em;
+    height: auto;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    font-size: .9em;
+  }
+}
+</style>
+
+<script lang="ts">
+import tagComponent from '@/components/components/tagComponent.vue'
+
+export default {
+  components: {
+    tagComponent
+  },
+  props: {
+    project: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
