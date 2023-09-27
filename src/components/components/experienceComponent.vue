5aa3f3382aada4404c2e718317e9ab7b5e9378cc
Robert Binkowski <56690174+RobertBinkowski@users.noreply.github.com>
2023-09-27 20:12:52 +0100

    error fix

--- src/components/components/experienceComponent.vue
+++ src/components/components/experienceComponent.vue
@@ -1,29 +1,28 @@
 <template>
-    <div class="wrapper">
-        <div v-show="experience.photo" class="image-experience">
-            <img :src="experience.photo" :alt="experience.name">
-        </div>
-        <div class="details-experience">
-            <a :href="experience.link">
-                <img :src="experience.icon" :alt="experience.name" />
-                <h3>{{ experience.name }}</h3>
-                <p>{{ experience.info }}</p>
-            </a>
-        </div>
+  <div class="wrapper">
+    <div v-show="experience.photo" class="image-experience">
+      <img :src="experience.photo" :alt="experience.name" />
     </div>
+    <div class="details-experience">
+      <a :href="experience.link">
+        <img :src="experience.icon" :alt="experience.name" />
+        <h3>{{ experience.name }}</h3>
+        <p>{{ experience.info }}</p>
+      </a>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 @import '@/assets/scss/variables';
 @import '@/assets/scss/mixins';
 
-
 .details-experience {
-    a {
-        img {
-            height: 5em;
-        }
+  a {
+    img {
+      height: 5em;
     }
+  }
 }
 </style>
 
@@ -31,7 +30,7 @@
 export default {
     props: {
         experience: {
-            type: object,
+            // type: object,
             required: true
         }
     }
