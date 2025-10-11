f76e9df936b461230e5f492abb36718c810320c0
Robert Krzysztof Binkowski <56690174+RobertBinkowski@users.noreply.github.com>
2025-10-12 00:56:26 +0100

    Updated Tags

---
+++ src/components/tagComponent.vue
@@ -0,0 +1,48 @@
+<template>
+  <a
+    :href="tag.link ?? '#'"
+    class="tag"
+    :style="{
+      backgroundColor: tag.color ? '#' + tag.color + '80' : 'gray',
+      borderColor: tag.color ? tag.color : 'darkgray',
+      borderRadius: tag.color ? '1em' : '0.3em',
+      cursor: tag.link ? 'pointer' : 'default',
+      // color: tag.color ? '#' + tag.color : 'white',
+    }"
+  >
+    <span
+      :style="{
+        color: tag.color ? tag.color : 'white',
+      }"
+      >#</span
+    >
+    <span class="tag-name">
+      {{ tag.name }}
+    </span>
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    tag: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@use '@/style/variables' as *;
+
+.tag {
+  margin: 0.5em;
+  padding: 0.3em 0.7em;
+
+  .tag-name {
+    margin-left: 0.5em;
+    color: white;
+  }
+}
+</style>
---
+++ src/layouts/03.LanguagesSection.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <tagComponent
+      class="tags"
+      v-for="language in languages"
+      :key="language"
+      :tag="language"
+    ></tagComponent>
+  </div>
+</template>
+
+<script>
+import tagComponent from '@/components/tagComponent.vue'
+
+export default {
+  components: {
+    tagComponent,
+  },
+  props: {
+    languages: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@use '@/style/variables' as *;
+
+div {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+
+  a {
+    font-size: 1.2em;
+  }
+}
+</style>
